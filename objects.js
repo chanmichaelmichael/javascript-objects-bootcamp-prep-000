12d2f8b38537085c43a82c0d59c849fe1d58e968
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 03:35:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,9 +2,8 @@ var playlist = {One, "One song"}
 //artist, title pair not necessarily lined updatePlaylist
 
 function updatePlaylist(playlist,name,title){
-  playlist.artist.push(name);
-  playlist.title.push(title);
-  return playlist;
+  playlist.name = title;
+  return playlist
 }
 
 function removeFromPlaylist(playlist, name){
